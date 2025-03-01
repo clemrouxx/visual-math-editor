7db7a00802d409a195e59cf0ed65b126c234f24d
Clément Roux <clemroux03@gmail.com>
2025-03-01 13:38:41 +0100

    added virtualkey with tooltip for shortcuts

--- src/VirtualKeyboard.js
+++ src/VirtualKeyboard.js
@@ -1,17 +1,29 @@
 import { MathJax } from "better-react-mathjax";
 import React from "react";
+import { Tooltip } from "react-tooltip";
 
 const VirtualKeyboard = ({ formulaEditorRef }) => {
+  const sym = "\\alpha";
+  const tooltip = "alp";
   return (
     <div class="virtual-keyboard">
         <MathJax>
-        <button onClick={() => formulaEditorRef.current?.addSymbol("\\alpha")}>
-            {"\\[\\alpha \\]"}
-        </button>
+            <VirtualKey symbol={sym} tooltip={tooltip} reference={formulaEditorRef}/>
         </MathJax>
     </div>
-    
   );
 };
+
+const VirtualKey = ({symbol,tooltip,reference}) => {
+  const tooltipId = symbol.replace(" ","__").replace("\\","--");// Necessary replacement to have a valid (but still unique) id
+  return (
+    <div>
+      <button data-tooltip-id={tooltipId} data-tooltip-content={tooltip} onClick={() => reference.current?.addSymbol(symbol)}>
+            {`\\[${symbol} \\]`}
+        </button>
+        <Tooltip id={tooltipId} />
+    </div>
+  );
+}
 
 export default VirtualKeyboard;
