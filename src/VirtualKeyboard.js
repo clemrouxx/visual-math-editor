730ed350c88b549f069061951bbf85b30a931f10
Clement <clemroux03@gmail.com>
2025-03-01 17:09:42 +0100

    full greek alphabet

--- src/VirtualKeyboard.js
+++ src/VirtualKeyboard.js
@@ -3,23 +3,25 @@ import React from "react";
 import { Tooltip } from "react-tooltip";
 import Keyboard from "./Keyboard";
 
+const GREEK_LETTERS = ["\\alpha","\\beta","\\gamma","\\Gamma","\\delta","\\Delta","\\epsilon","\\varepsilon","\\zeta","\\eta","\\theta","\\vartheta","\\Theta","\\iota","\\kappa","\\varkappa","\\lambda","\\Lambda","\\mu","\\nu","\\xi","\\Xi","\\pi","\\Pi","\\rho","\\varrho","\\sigma","\\Sigma","\\tau","\\upsilon","\\Upsilon","\\phi","\\varphi","\\Phi","\\chi","\\psi","\\Psi","\\omega","\\Omega"];
+
 const VirtualKeyboard = ({ formulaEditorRef }) => {
   const reversedShortcuts = Object.fromEntries(Object.entries(Keyboard.SHORTCUTS).map(([key, value]) => [value, key]));
-  const symblist = ["\\alpha","\\beta","\\gamma"];
   return (
     <MathJax>
     <div className="virtual-keyboard">
-        
-            {symblist.map((symbol, index) => (
-              <VirtualKey symbol={symbol} tooltip={reversedShortcuts[symbol]} reference={formulaEditorRef} key={index}/>
-            ))}
-
+      <h3>Greek letters</h3>
+      <div className="key-row">
+              {GREEK_LETTERS.map((symbol, index) => (
+                <SymbolVirtualKey symbol={symbol} tooltip={`[${reversedShortcuts[symbol]}]`} reference={formulaEditorRef} key={index}/>
+              ))}
+      </div>
     </div>
     </MathJax>
   );
 };
 
-const VirtualKey = ({symbol,tooltip,reference}) => {
+const SymbolVirtualKey = ({symbol,tooltip,reference}) => {
   const tooltipId = symbol.replace(" ","__").replace("\\","--");// Necessary replacement to have a valid (but still unique) id
   return (
     <div>
