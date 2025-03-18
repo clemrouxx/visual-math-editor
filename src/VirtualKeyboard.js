8613fde2b31e0859acf0778dacd7166eac91489a
clemrouxx <clemroux03@gmail.com>
2025-03-18 15:52:52 +0100

    added trace in functions

--- src/VirtualKeyboard.js
+++ src/VirtualKeyboard.js
@@ -21,7 +21,7 @@ const ORDER = ["\\leq","\\geq","\\ll","\\gg","\\subset","\\supset","\\subseteq",
 
 const CONSTRUCTS = ["\\sum","\\prod","\\int","\\iint","\\iiint","\\oint","\\bigcup","\\bigcap","\\bigoplus","\\bigotimes","\\overbrace","\\underbrace","\\widehat","\\overrightarrow","\\ket","\\bra","\\bigwedge","\\bigvee","\\bigodot","\\biguplus","\\iiiint","\\idotsint","\\widetilde","\\overleftarrow","\\overline","\\underline","\\overleftrightarrow","\\underleftrightarrow","\\underleftarrow","\\underrightarrow","\\ulcorner","\\llcorner","\\overset","\\underset","inverse","transpose","updagger"];
 
-const NAMED_FUNCTIONS = ["\\exp","\\log","\\min","\\max","\\arg","\\lim","\\cos","\\sin","\\tan","\\arccos","\\arcsin","\\arctan","\\cosh","\\sinh","\\tanh","\\det","\\ker","\\inf","\\sup","\\deg","\\cot","\\sec","\\csc","\\dim","\\gcd","\\hom","\\lg","\\liminf","\\limsup","\\Pr","\\sup","\\injlim","\\projlim","\\varinjlim","\\varprojlim","\\varliminf","\\varlimsup"];
+const NAMED_FUNCTIONS = ["\\exp","\\log","\\min","\\max","\\arg","\\lim","\\cos","\\sin","\\tan","\\arccos","\\arcsin","\\arctan","\\cosh","\\sinh","\\tanh","\\det","\\ker","\\inf","\\sup","\\deg","\\cot","\\sec","\\csc","\\dim","\\gcd","\\hom","\\lg","\\liminf","\\limsup","\\Pr","\\sup","\\injlim","\\projlim","\\varinjlim","\\varprojlim","\\varliminf","\\varlimsup","\\operatorname{Tr}","\\operatorname{tr}"];
 
 const VirtualKeyboard = ({ formulaEditorRef }) => {
   const reversedShortcuts = Object.fromEntries(Object.entries(MathKeyboard.SHORTCUTS).map(([key, value]) => [value, key]));
